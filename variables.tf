92fb582473b91c5ffe0a672453066b6279ac7e5d
Tim Curless <tim.curless@ahead.com>
2025-09-18 12:59:59 -0500

    variable update

--- variables.tf
+++ variables.tf
@@ -5,7 +5,7 @@ variable "template_org" {
 
 variable "template_repo" {
   description = "The name of the repository in Github that contains the example app code."
-  default     = "learn-hcp-waypoint-static-app-template"
+  default     = "terraform-github-static-app"
 }
 
 variable "destination_org" {
