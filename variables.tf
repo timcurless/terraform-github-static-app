565d3be4225d3f5fc366ad55fbe826f1d817c79e
Tim Curless <tim.curless@ahead.com>
2025-09-18 13:15:30 -0500

    more var updates

--- variables.tf
+++ variables.tf
@@ -1,6 +1,6 @@
 variable "template_org" {
   description = "The name of the organization in Github that will contain the example app repo."
-  default     = "hashicorp-demo"
+  default     = "hashicorp-education"
 }
 
 variable "template_repo" {
@@ -10,7 +10,7 @@ variable "template_repo" {
 
 variable "destination_org" {
   description = "The name of the organization in Github that will contain the templated repo."
-  default     = "hashicorp-demo"
+  default     = "timcurless"
 }
 
 variable "gh_token" {
